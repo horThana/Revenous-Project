eab6d65449b0ec51a89e5fac3a9700fbcc848909
horThana <thanawat.maphet@gmail.com>
2023-11-11 23:59:03 +0700

    mockdata and prepare to connect api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import styles from "./App.module.css";
 
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
+import axios from "axios";
+import businesses from './mockData';
 
-const business = {
-  imageSrc:
-    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Bordertown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
+// const business = {
+//   imageSrc:
+//     "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+//   name: "MarginOtto Pizzeria",
+//   address: "1010 Paddington Way",
+//   city: "Bordertown",
+//   state: "NY",
+//   zipCode: "10101",
+//   category: "Italian",
+//   rating: 4.5,
+//   reviewCount: 90,
+// };
 
-const businesses = [business, business, business, business, business, business];
+// const businesses = [business, business, business, business, business, business];
 
 const App = () => {
+  const [business, setBusinesses] = useState([]);
+  
+  useEffect(() => {
+      fetchBusinesses();
+  }) 
+
+  const fetchBusinesses = async () => {
+    try{
+      const response = await axios.get('API_URL');
+      setBusinesses(response.data.businesses);
+    }catch(error) {
+      console.log(error);
+    }
+  }
+
+
   return (
     <div className={styles.App}>
-      <h1>ravenous</h1>
+      <h1>ALL Ravenous</h1>
       <SearchBar />
       <BusinessList businesses={businesses} />
     </div>
---
+++ src/components/App/mockData.js
@@ -0,0 +1,29 @@
+
+const businesses = [
+  {
+    imageSrc: "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+    name: "Business 1",
+    address: "Address 1",
+    city: "City 1",
+    state: "State 1",
+    zipCode: "Zip 1",
+    category: "Category 1",
+    rating: 4.5,
+    reviewCount: 90,
+  },
+  {
+    imageSrc: "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+    name: "Business 1",
+    address: "Address 1",
+    city: "City 1",
+    state: "State 1",
+    zipCode: "Zip 1",
+    category: "Category 1",
+    rating: 4.5,
+    reviewCount: 90,
+  },
+  // Add more
+
+];
+
+export default businesses;
